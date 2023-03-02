95de7468f6e37b300eb08ef07a0f01c1b697acc4
trajectories <79078599+trajectories@users.noreply.github.com>
2023-03-02 00:40:00 +0000

    Create __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,48 @@
 from mycroft import MycroftSkill, intent_file_handler
+import requests
+import json
+
+url = "https://mindx.mind.ai/api/v1/gateway/default/u0W0dvyUR9K_pUX5-bEZlg/2ILfgsKlSHi7bcFDc8DPnQ"
+api_key = "kq2kYszSQGesoMhXZfmpXA"
 
 
 class LeRestaurant(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
+        
 
     @intent_file_handler('restaurant.le.intent')
     def handle_restaurant_le(self, message):
         self.speak_dialog('restaurant.le')
+        self.headers = {
+        	"Content-Type": "application/json",
+        	"Authorization": "Bearer {api_key}",
+        }
+        self.speak('this is my statement')
+        data = {"query": "hi"}
+        response = requests.post(url, headers=self.headers, json=data)
+        response_data = response.json()
+        for channel_result in response_data.get('data', {}).get('channel-result', []):
+        	for channel_message in channel_result.get('channel-message', []):
+        		if channel_messasge.get('templete-type') == 'text':
+        			message = channel_message.get('templete')
+        print("=========>MindExpresssssssssssssssssssion", message)
+"""
+    def handle_mind_expression_query(self, message):
+    	self.headers = {
+        	'Content-Type': 'application/json',
+        	'Authorization': {self.api_key},
+        }
+    	self.query = message.date.get('query')
+    	self.data = {
+    		'query': {self.query}
+    	}
+	response = requests.post(self.url, headers=self.headers, json=self.data)
+	response_data = response.json().["data"]
+	print("MindExpresssssssssssssssssssion", response_data)
+"""
 
 
 def create_skill():
     return LeRestaurant()
-
