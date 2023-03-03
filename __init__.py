e1b3d7248d8a282b8b7336b4a98ef4433d29fdb4
trajectories <79078599+trajectories@users.noreply.github.com>
2023-03-03 10:52:16 +0700

    __init__.py

--- __init__.py
+++ __init__.py
@@ -5,27 +5,31 @@ import json
 url = "https://mindx.mind.ai/api/v1/gateway/default/u0W0dvyUR9K_pUX5-bEZlg/2ILfgsKlSHi7bcFDc8DPnQ"
 api_key = 'kq2kYszSQGesoMhXZfmpXA'
 headers = {
-	"Content-Type": "application/json",
-	"Authorization": "Bearer "+api_key,
+    "Content-Type": "application/json",
+    "Authorization": "Bearer "+api_key,
 }
 
-class LeRestaurant(MycroftSkill): 
-	def __init__(self): 
-		MycroftSkill.__init__(self)
 
-	@intent_file_handler('restaurant.le.intent')
-	def handle_restaurant_le(self, message):
-		# welcome!
-		self.speak_dialog('restaurant.le') 
-		handle_send_message_to_mind_expression()
+class LeRestaurant(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
 
-	def handle_send_message_to_mind_expression(self): query = "สวัสดี" 
-		data = {"query": query} response = requests.post(url, 
-		headers=headers, json=data) response_data = response.json() 
-		templete = 
-		response_data['data']['channel-result'][0]['channel-message']['template'] 
-		conversation_id = response_data['data']['conversation_id'] 
-		self.speak(templete) self.speak(conversation_id)
+    @intent_file_handler('restaurant.le.intent')
+    def handle_restaurant_le(self, message):
+        # welcome!
+        self.speak_dialog('restaurant.le')
+        self.handle_send_message_to_mind_expression()
 
-def create_skill(): 
-	return LeRestaurant()
+    def handle_send_message_to_mind_expression(self):
+        query = "สวัสดี"
+        data = {"query": query}
+        response = requests.post(url, headers=headers, json=data)
+        response_data = response.json()
+        templete = response_data['data']['channel-result'][0]['channel-message']['template']
+        conversation_id = response_data['data']['conversation_id']
+        self.speak(templete)
+        self.speak(conversation_id)
+
+
+def create_skill():
+    return LeRestaurant()
